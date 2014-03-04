cb22ac217c7f67c4beb90f4de5e863f500ef435e
Walter Murphy <walter.murphy@gmail.com>
2014-03-03 20:48:02 -0800

    Convert 2 parameters to globals to reduce stack load in recursive function

--- dtree/print_tree_gv.py
+++ dtree/print_tree_gv.py
@@ -8,8 +8,13 @@ label_fmt  = "\"%s\" -> \"%s\" [label = \"%s\"]\n"
 box_fmt    = "\"%s\" [shape = \"box\"]\n"
 circle_fmt = "\"%s\" [shape = \"circle\"]\n"
 
+# These globals are used to reduce stack load in a recursive function.
+fout = 0
+leaf = ""
 
-def print_tree_gv(tree, fout, str):
+def print_tree_gv(tree, fn_out, collapse, str):
+    global fout
+    fout = fn_out
     fout.write("digraph %s {\n\n" % str)
 
     # Collect attributes so their shapes can be pre-declared
@@ -26,7 +31,7 @@ def print_tree_gv(tree, fout, str):
         fout.write(circle_fmt % item)
     fout.write('\n')
 
-    print_dotworker(tree, fout, None, None, None)
+    print_dotworker(tree, fout, None)
     fout.write("\n}\n")
 
 
@@ -45,7 +50,12 @@ def print_dotleaf(tree, leaves):
         leaves.append(tree)
 
 
-def print_dotworker(tree, fout, attr, value, leaf):
+def print_dotworker(tree, attr, value):
+    # Of course globals are usually a bad idea, but since this is a 
+    # recursive function and stack size matters, no parameters that 
+    # don't change should be on the stack. The only reason 'leaf' can
+    # be a global is because it's always a terminal node.
+    global fout, leaf
     if type(tree) == dict:
         attr_old = attr
         attr = tree.keys()[0]
@@ -56,9 +66,9 @@ def print_dotworker(tree, fout, attr, value, leaf):
                 fout.write(label_fmt % (attr, leaf, value))
         for item in tree.values()[0].keys():
             value = item
-            print_dotworker(tree.values()[0][item], fout, attr, value, leaf)
+            print_dotworker(tree.values()[0][item], attr, value)
     else:
         leaf = tree
         if value != None:
             fout.write(label_fmt % (attr, leaf, value))
-
+        leaf = None
